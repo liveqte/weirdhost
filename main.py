fa6087ac5b2c0bbcfaa58e48bf8496873dba7fd1
liveqte <3571650235@qq.com>
2025-12-14 15:26:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def add_server_time(server_url="https://hub.weirdhost.xyz/server/7b40aae4"):
 
 if __name__ == "__main__":
     print("开始执行添加服务器时间任务...")
-    server_url=os.getenv('WEIRDHOST_SERVER_URLS')
+    server_url=os.environ.get('WEIRDHOST_SERVER_URLS')
     success = add_server_time(server_url)
     if success:
         print("任务执行成功。")
