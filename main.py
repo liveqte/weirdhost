86c1fb2b9b2267338c235c171a4ed8776f34238d
liveqte <3571650235@qq.com>
2025-12-27 12:22:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 from playwright.sync_api import sync_playwright, Cookie, TimeoutError as PlaywrightTimeoutError
 from typing import Optional
+import re
 
 def is_valid_proxy(proxy: str) -> bool:
     """
